686a28593de3ad668d1472719c438404e1a5e316
nathbout2 <nathbout2@gmail.com>
2021-11-04 11:58:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <title>FoodApp</title>
+</head>
+<body>
+<nav class="navbar">
+    <img src="images/logo.svg" width="30" height="30" alt="">
+    <img src="icons/menu.svg" width="30" height="30" alt="">
+  </a>
+</nav>
+<div class="text_head">
+        <h1>Beautiful food & takeaway,<span>delivered</span> to your door</h1>
+    </div>
+ <div class ="head">
+    <div class="description_head">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor volutpat massa
+        aliquam mollis. Curabitur mattis ligula elit, eu congue purus interdum ut. Pellentesque egestas nisi eu quam
+        finibus suscipit. Aliquam at orci pharetra, malesuada tortor vel, tincidunt felis. Nulla sollicitudin sapien et urna sagittis d
+        apibus. Ut aliquam erat ante
+        </p>
+    </div>
+    <div class="btn_head">
+        <button>Place an Order</button>
+    </div>
+    <div class="img_head">
+        <img src="images/hero_image.png" class="img" alt="" srcset="">
+    </div>
+ </div>
+ <div class="mid">
+ <div class="text_mid">
+        <h1 class="title">The home of fresh products</h1>
+    </div>
+    <div class="description_mid">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor volutpat massa
+        aliquam mollis. Curabitur mattis ligula elit, eu congue purus interdum ut. Pellentesque egestas nisi eu quam
+        finibus suscipit. Aliquam at orci pharetra, malesuada tortor vel, tincidunt felis. Nulla sollicitudin sapien et urna sagittis d
+        apibus. Ut aliquam erat ante
+        </p>
+    </div>
+    <div class="btn_mid">
+        <button>Learn about us</button>
+    </div>
+    <div class="img_mid">
+        <img src="images/eat.png" class="img" alt="" srcset="">
+    </div>
+ </div>
+ <div class ="end">
+     <h1 class="title">How it works.</h1>
+     <div class="part_one">
+         <img src="images/step_01.png"  class="img"alt="" srcset="">
+         <div class="text_part_one">
+             <h2>Adapt your menu items</h2>
+             <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+         </div>
+     </div>
+     <div class="part_two">
+         <img src="images/step_02.png"  class="img" alt="" srcset="">
+         <div class="text_part_two">
+             <h2> Accept online orders & takeout</h2>
+             <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+         </div>
+     </div>
+     <div class="part_three">
+         <img src="images/step_03.png" class="img" alt="" srcset="">
+         <div class="text_part_three">
+             <h2> Manage delivery or takeout</h2>
+             <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+         </div>
+     </div>
+ </div>
+ <div class="footer">
+     <div class="txt_footer">
+         <h4>Powered by FoodApp</h4>
+         <p class="text_footer_two">CGV    About us</p>
+     </div>
+ </div>
+</body>
+</html>
